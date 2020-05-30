1a740ae6c46597fb4389006edbbc6ce851231bad
kouzana fedi <fedi.kouzana1@gmail.com>
2020-05-30 18:18:35 +0100

    Setup project

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ This branch will be used to setup the project structure.
 
 - [Vue.js](https://vuejs.org/)
 - [Electron.js](https://www.electronjs.org/)
+
+## Scripts
+
+- launch up in browser : npm run serve
+- launch up with Electron : npm run serve:electron
---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+   <Home />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Home from '@/views/Home.vue'
+export default Vue.extend({
+  name:'app',
+  components:{
+    Home
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
